5da6810b558e5394f0e435533a49e9c771405702
mochammad.yoga <mochammad.yoga@tpfx.co.id>
2026-07-28 19:06:55 +0700

    feat: add service worker to enable cross-origin isolation headers

---
+++ public/coi-serviceworker.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+        return;
+    }
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                if (response.status === 0) {
+                    return response;
+                }
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            })
+            .catch((e) => {
+                console.error(e);
+            })
+    );
+});
