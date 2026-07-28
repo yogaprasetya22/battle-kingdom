45c80ea30276955e2f80fdb16ce8e2e93854558e
mochammad.yoga <mochammad.yoga@tpfx.co.id>
2026-07-28 19:08:52 +0700

    feat: refactor coi-serviceworker to support automatic registration and reload on updates

--- public/coi-serviceworker.js
+++ public/coi-serviceworker.js
@@ -1,27 +1,39 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+if (typeof window === "undefined") {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 
-self.addEventListener("fetch", (event) => {
-    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
-        return;
-    }
-    event.respondWith(
-        fetch(event.request)
-            .then((response) => {
-                if (response.status === 0) {
-                    return response;
-                }
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                return new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders,
-                });
-            })
-            .catch((e) => {
-                console.error(e);
-            })
-    );
-});
+    self.addEventListener("fetch", (event) => {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+        event.respondWith(
+            fetch(event.request)
+                .then((response) => {
+                    if (response.status === 0) {
+                        return response;
+                    }
+                    const newHeaders = new Headers(response.headers);
+                    newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+                    newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                    return new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: newHeaders,
+                    });
+                })
+                .catch((e) => {
+                    console.error(e);
+                })
+        );
+    });
+} else {
+    // Register service worker using the script's own URL (currentScript.src)
+    navigator.serviceWorker.register(window.document.currentScript.src).then((registration) => {
+        registration.addEventListener("updatefound", () => {
+            window.location.reload();
+        });
+        if (registration.active && !navigator.serviceWorker.controller) {
+            window.location.reload();
+        }
+    });
+}
